0953abcb39688c1e7509098cfa589786fcc90479
prembudda <premkumarneerukonda@10.0.0.155>
2019-09-13 20:45:06 -0700

    push to git

---
+++ eclipse-workspace/mavendemo/src/main/java/mavendemo/App.java
@@ -0,0 +1,14 @@
+package mavendemo;
+
+public class App {
+   public String Sample() {
+	   System.out.println("kiran");
+	return null;
+}
+   
+   public static void main(String[] args) {
+	   App ap = new App();
+	   ap.Sample();
+   }
+
+}
